d543ad1f5ff5ac4d059290055ac1a95330d7da56
Sher-Kon <sher-kon@ya.ru>
2022-03-10 15:50:12 +0300

    начал централизованно обрабатывать ошибки

--- controllers/cards.js
+++ controllers/cards.js
@@ -44,6 +44,9 @@ module.exports.deleteCard = (req, res) => {
     .then((card) => { //
       if (!card) {
         res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
+        // const err = new Error('Карточка с указанным _id не найдена');
+        // err.statusCode = 404;
+        // next(err);
       }
       if (JSON.stringify(card.owner) === `"${req.user._id}"`) {
         // res.send({ message: 'Своя карточка' });
@@ -53,14 +56,23 @@ module.exports.deleteCard = (req, res) => {
           });
       } else {
         res.send({ message: 'Нельзя удалять чужую карточку' });
+        // const err = new Error('Нельзя удалять чужую карточку');
+        // err.statusCode = 400;
+        // next(err);
       }
     })
     .catch((err) => { //
       // console.dir(err);
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Невалидный id ' });
+        // const err = new Error('Невалидный id');
+        // err.statusCode = 400;
+        // next(err);
       } else {
         res.status(500).send({ message: 'Карточка с указанным _id не найдена' });
+        // const err = new Error('Карточка с указанным _id не найдена');
+        // err.statusCode = 500;
+        // next(err);
       }
     });
 };

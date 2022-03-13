d4658360f218dd79b9156b3585ef7a91b62708e1
Шерстнев Константин Адольфович <Sher@s-tech.ru>
2022-03-13 18:32:38 +0300

    приступил к валидации поступающих данных

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routerCards = require('./routes/cards'); // импортируем роутер
 const routerUsers = require('./routes/users'); // импортируем роутер
-
-const {
-  createUser,
-  login,
-} = require('./controllers/users');
+const {createUser, login,} = require('./controllers/users');
 const auth = require('./middlewares/auth');
+
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -19,8 +18,23 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+//app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+
+//app.post('/signup', createUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }).unknown(true),
+}), createUser);
+
+
 // авторизация
 app.use(auth);
 
@@ -33,6 +47,8 @@ app.use((req, res, next) => {
   next(err);
 });
 
+// обработчики ошибок
+app.use(errors()); // обработчик ошибок celebrate
 // централизованно обрабатываем все ошибки
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
